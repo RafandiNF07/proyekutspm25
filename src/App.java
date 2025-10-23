1f86dfc133baa22f1f9e4b0f1a82c80bf309dde8
Rafandi NF <126743650+RafandiNF07@users.noreply.github.com>
2025-10-23 16:30:53 +0700

    perubahan 2

--- src/App.java
+++ src/App.java
@@ -2,6 +2,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Heltetsttstst!");
+        System.out.println("Hello, Universe!");
+        System.out.println("Hello, Multiverse!");
     }
 }
 //test tolong
+// test lagi
